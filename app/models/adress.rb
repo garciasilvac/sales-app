994c24e169c83e2e1f9cd1f3074bd93a39fe0ecb
Carlos Garcia <garciasilvac@uc.cl>
2020-05-20 16:37:13 -0400

    client and adress translations

--- app/models/adress.rb
+++ app/models/adress.rb
@@ -2,4 +2,9 @@ class Adress < ApplicationRecord
 belongs_to :client
 
 validates :client_id, presence: true
+
+def full_adress
+	self.street+" "+self.str_number.to_s+", "+self.adr_county+", "+self.adr_state
+end
+
 end
