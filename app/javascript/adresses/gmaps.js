dcd121d451258aef8f0c39d9557fb030c8b57fc4
Carlos Garcia <garciasilvac@uc.cl>
2020-06-01 12:08:38 -0400

    map layout

--- app/javascript/adresses/gmaps.js
+++ app/javascript/adresses/gmaps.js
@@ -1,4 +1,4 @@
-export function initMap(lat, lng, mapDiv) {
+export function initMap(lat, lng, mapDiv, iwContent) {
     var myCoords = new google.maps.LatLng(lat, lng);
     var mapOptions = {
     center: myCoords,
@@ -8,6 +8,14 @@ export function initMap(lat, lng, mapDiv) {
     var map = new google.maps.Map(mapDiv, mapOptions);
     var marker = new google.maps.Marker({
         position: myCoords,
-        map: map
+        map: map,
+        clickable : true
+    });
+    marker.info = new google.maps.InfoWindow({
+        content: iwContent
+    });
+    marker.info.open(map, marker);
+    google.maps.event.addListener(marker, 'click', function() {
+      marker.info.open(map, marker);
     });
 }
