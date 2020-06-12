a637d5e0109c7f1a4764e5eabaf543e9b400d79d
Carlos Garcia <garciasilvac@uc.cl>
2020-06-12 12:24:10 -0400

    modification to migration for set deleted to false in all existing records

--- db/migrate/20200612151610_add_deleted_to_models.rb
+++ db/migrate/20200612151610_add_deleted_to_models.rb
@@ -3,5 +3,20 @@ class AddDeletedToModels < ActiveRecord::Migration[6.0]
   	add_column :adresses, :deleted, :boolean
   	add_column :clients, :deleted, :boolean
   	add_column :users, :deleted, :boolean
-  end
+
+  	Adress.update_all(deleted: false)
+  	Client.update_all(deleted: false)
+  	Delivery.update_all(deleted: false)
+  	DeliveryType.update_all(deleted: false)
+  	PaymentType.update_all(deleted: false)
+  	PointOfSale.update_all(deleted: false)
+  	Product.update_all(deleted: false)
+  	Sale.update_all(deleted: false)
+  	ShoppingCart.update_all(deleted: false)
+  	Subtype.update_all(deleted: false)
+  	Timeblock.update_all(deleted: false)
+  	Type.update_all(deleted: false)
+  	User.update_all(deleted: false)
+  end 
+
 end
