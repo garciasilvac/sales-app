000a6346f790e0f5c0a3cf102a6d0ea76433bdb6
Carlos Garcia <garciasilvac@uc.cl>
2020-08-20 21:56:53 -0400

    migration fixed

--- db/migrate/20200820233742_add_discount_to_shopping_cart.rb
+++ db/migrate/20200820233742_add_discount_to_shopping_cart.rb
@@ -1,10 +1,10 @@
 class AddDiscountToShoppingCart < ActiveRecord::Migration[6.0]
-  def change
-    add_column :shopping_carts, :discount, :float
-  end
-
   def up
+    add_column :shopping_carts, :discount, :float
     ShoppingCart.update_all(discount:0)
+  end
+  def down
+    remove_column :shopping_carts, :discount, :float
   end
   
 end
